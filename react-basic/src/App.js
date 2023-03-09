9a61080aef9cbf12038b007cc6c1ccdc399a3a46
kwanruk <kwantuk@gmail.com>
2023-03-09 08:18:39 +0700

    First time git hub

---
+++ react-basic/src/App.js
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+   <div>
+    <h1>Program Receive Payment</h1>
+   <p>Save receive and payment</p>
+   <ul>
+<li>ค่าเดินทาง <span>-200</span></li>
+<li>เงินเดือน <span>+20000</span></li>
+<li>อาหาร <span>-500</span></li>
+
+   </ul>
+   </div>
+  );
+}
+
+export default App;
